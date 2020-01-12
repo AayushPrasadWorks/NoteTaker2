0ca0e8534ed5ebe1ddcbb590ca3df7c1a8d179d3
aayushprasad <reach.aayushp@gmail.com>
2020-01-12 11:39:36 -0500

    finished server

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 import pymongo
 
 myclient = pymongo.MongoClient('localhost', 27017)
-mydb = myclient["test"]
-mycol = mydb["test"]
+db = myclient["test"]
+cl = mydb["test"]
 
 
-mydict = { "name": "John", "message": "hello", "date": "00/00/00"}
+dicts = { "name": "John", "message": "hello", "date": "00/00/00"}
 
-x = mycol.insert_one(mydict)
-y =  mycol.find({},{ "name": 1, "message": 1 })
+x = cl.insert_one(dicts)
+y =  cl.find({},{ "name": 1, "message": 1 })
 print(x)
 print(y)
